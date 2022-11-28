23dd6e401d700031b7ce8ffc2ea97630c878e731
RiskiMaulana27 <119285345+RiskiMaulana27@users.noreply.github.com>
2022-11-28 14:43:11 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main(){
+ int angka[]={3,5,6,8,12};
+ cout<< "angka" << angka[4] <<endl;
+ 
+   char huruf[]={'x','y','z'};
+   printf ("Huruf %c\n",huruf[1]);
+   string nama[]= {"Riski","Zia","Pandu"};
+   cout <<"nama="<<nama[0]<<endl;
+}
